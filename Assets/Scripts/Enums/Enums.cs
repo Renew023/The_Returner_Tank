5a38aa6bc321363b2c9e28fdcde0fb27dc339067
ParkJWoo <character453@naver.com>
2025-05-09 17:08:13 +0900

    feat: StageType 추가

--- Assets/Scripts/Enums/Enums.cs
+++ Assets/Scripts/Enums/Enums.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public enum StageType
 {
     NormalBattle,       //  일반 전투 스테이지
-    BossBattle          //  보스 전투 스테이지
+    BossBattle,          //  보스 전투 스테이지
+    Rest
 }
 
 public class Enums : MonoBehaviour
