91708c6162eb2a569bd503d1079b3806e4637c5e
Renew023 <ya1539@naver.com>
2025-05-10 15:37:30 +0900

    feat. 가장 가까운 적 공격 기능 추가
    feat. 가장 가까운 적 공격 기능 추가

---
+++ Assets/Scripts/Weapon/AttackTarget.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.EventSystems;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using static UnityEngine.GraphicsBuffer;
+
+[ExecuteInEditMode]
+public class AttackTarget : MonoBehaviour
+{
+	public float radius = 3.0f;
+	public LayerMask layer;
+	public Collider2D[] colliders;
+	public Collider2D short_enemy;
+
+	public Vector2 Searching()
+	{
+		//Collider[] colliders;
+		//Collider short_enemy;
+		colliders = Physics2D.OverlapCircleAll(transform.position, radius, layer);
+
+		if (colliders.Length > 0)
+		{
+			float short_distance = Vector2.Distance(transform.position, colliders[0].transform.position);
+			short_enemy = colliders[0];
+			foreach (Collider2D col in colliders)
+			{
+				float short_distance2 = Vector3.Distance(transform.position, col.transform.position);
+				if (short_distance > short_distance2)
+				{
+					short_distance = short_distance2;
+					short_enemy = col;
+				}
+			}
+			return short_enemy.transform.position;
+		}
+		return Vector2.zero;
+	}
+	
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, radius);
+	}
+}
