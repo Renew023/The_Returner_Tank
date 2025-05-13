9831f864f0143670eddd5e272b1da32c5f4adf5c
leesoomang <soo199617@gmail.com>
2025-05-13 10:17:40 +0900

    맵스테이지 선택씬 초기화

--- Assets/Scripts/Map/NodeController.cs
+++ Assets/Scripts/Map/NodeController.cs
@@ -1,21 +1,31 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+
+/// 각 노드(UI Image + Button)에 붙여서 클릭을 MapManager에 전달.
+
 [RequireComponent(typeof(Button))]
+
 public class NodeController : MonoBehaviour
 {
-    [Header("노드별 크기")]
+    [Header("노드별 크기 (Size Delta)")]
     public Vector2 enemySize = new Vector2(64, 64);
     public Vector2 healSize = new Vector2(80, 80);
     public Vector2 bossSize = new Vector2(120, 120);
 
-    int row, col;
-    NodeType type;
-    MapManager mapMgr;
-    Image img;
+    private Vector3 originalScale;
+
+    private int row, col;
+    private NodeType type;
+    private MapManager mapMgr;
+    private Image img;
 
     public int Row => row;
     public int Col => col;
+
+
+
+    /// 맵매니저가 생성 직후 초기화
 
     public void Init(int r, int c, NodeType t, MapManager mgr, Sprite icon, byte alpha)
     {
@@ -24,29 +34,51 @@ public class NodeController : MonoBehaviour
         type = t;
         mapMgr = mgr;
 
+        // 아이콘 교체 & 기본 투명도 설정
         img = GetComponent<Image>();
         img.sprite = icon;
-        var color = img.color;
-        color.a = alpha / 255f;
-        img.color = color;
+        var ccol = img.color;
+        ccol.a = alpha / 255f;
+        img.color = ccol;
 
-        var btn = GetComponent<Button>();
-        btn.onClick.RemoveAllListeners();
-        btn.onClick.AddListener(() => mapMgr.OnNodeClicked(row, col, type));
+        // 클릭 콜백
+        GetComponent<Button>().onClick.AddListener(OnClick);
 
         var rt = GetComponent<RectTransform>();
-        switch (type)
+        switch (t)
         {
-            case NodeType.Enemy: rt.sizeDelta = enemySize; break;
-            case NodeType.Heal: rt.sizeDelta = healSize; break;
-            case NodeType.Boss: rt.sizeDelta = bossSize; break;
+            case NodeType.Enemy:
+                rt.sizeDelta = enemySize;
+                break;
+            case NodeType.Heal:
+                rt.sizeDelta = healSize;
+                break;
+            case NodeType.Boss:
+                rt.sizeDelta = bossSize;
+                break;
         }
+
+        originalScale = rt.localScale;
     }
 
+    private void OnClick()
+    {
+        mapMgr.OnNodeClicked(row, col, type);
+    }
+
+    /// 외부에서 투명도만 바꿀 때
     public void SetAlpha(byte alpha)
     {
         var c = img.color;
         c.a = alpha / 255f;
         img.color = c;
+    }
+
+    /// 외부에서 현재 선택 위치 표시용
+    public void SetHighlight(bool on)
+    {
+        img.color = on
+            ? new Color(img.color.r, img.color.g, img.color.b, 1f)
+            : new Color(img.color.r, img.color.g, img.color.b, img.color.a);
     }
 }
